c765490e498c52cf058de7d32c13248f21010b96
anishsingh20 <loveeanish@gmail.com>
2021-05-25 14:12:03 +0530

    challeges for chapter 3

---
+++ Chapter3/Challenges/args.sh
@@ -0,0 +1,8 @@
+#/usr/local/bin/bash
+
+
+for i in {1..EOF}
+do
+	echo $i
+
+done
---
+++ Chapter3/Challenges/assign.sh
@@ -0,0 +1,8 @@
+#!/usr/local/bin/bash
+
+File=${TheFile:-/tmp/data.file}
+
+TheFile="This is Anish"
+echo $TheFile
+echo $File
+
